f1b80a8406ac8e9529b5fccf6936777934dbdc6e
haldamir95 <haldamir.95@gmail.com>
2024-03-11 20:34:16 -0600

    fix: solved remove item bug that deleted all the array

--- components/OrdersTable.tsx
+++ components/OrdersTable.tsx
@@ -39,65 +39,68 @@ const OrdersTable = () => {
             label: "ACCION",
         },
     ];
-    const rows: Array<OrderRow> = []
+    let rows: Array<OrderRow> = []
 
 
     useEffect(() => {
         pubnub.subscribe({ channels: ['whichoosChannel'] });
         pubnub.addListener({
-            message: (event:any) => {
-                const newOrder:Order = JSON.parse(event.message.content);
-                if(!orderExist(newOrder.id!)){
-                    const orderRow:OrderRow={
-                        id:newOrder.id,
-                        key:newOrder.id!,
-                        size:newOrder.size,
-                        drink:newOrder.drink,
-                        complement:newOrder.complement,
-                        type:newOrder.type,
-                        sugar_type:newOrder.sugar_type,
-                        sugar_qty:newOrder.sugar_qty,
-                        chai:newOrder.chai,
-                        infusion:newOrder.infusion
+            message: (event: any) => {
+                const newOrder: Order = JSON.parse(event.message.content);
+                if (!orderExist(newOrder.id!)) {
+                    const orderRow: OrderRow = {
+                        id: newOrder.id,
+                        key: newOrder.id!,
+                        size: newOrder.size,
+                        drink: newOrder.drink,
+                        complement: newOrder.complement,
+                        type: newOrder.type,
+                        sugar_type: newOrder.sugar_type,
+                        sugar_qty: newOrder.sugar_qty,
+                        chai: newOrder.chai,
+                        infusion: newOrder.infusion
                     }
                     rows.push(orderRow)
-                    setOrdersList(prevOrdersList => [...prevOrdersList, orderRow]);
+                    setOrdersList([...ordersList, orderRow]);
                 }
             }
         });
         return () => {
             pubnub.unsubscribeAll();
         }
-        
-    },[])
-    
-    const orderExist = (id:string):boolean => {
-        return rows.some((order:OrderRow)=>order.id===id)
+
+    }, [ordersList])
+
+    const orderExist = (id: string): boolean => {
+        return rows.some((order: OrderRow) => order.id === id)
     }
 
-    const removeFromList = (id:string) =>{
-        const newOrdersList = rows.filter((order) => order.key != id);
-        setOrdersList(newOrdersList)
+    const removeFromList = (id: string) => () => {
+        const filteredList = ordersList.filter( order => order.key !== id)
+        rows = filteredList
+        setOrdersList(filteredList)
     }
 
-    
+
+
+
     return (
         <>
             <Table isStriped aria-label="Table to see the orders">
                 <TableHeader columns={columns}>
                     {(column) => <TableColumn key={column.key}>{column.label}</TableColumn>}
                 </TableHeader>
-                <TableBody items={ordersList}>
-                    {(item) => (
+                <TableBody>
+                    {ordersList.map((item) =>
                         <TableRow key={item.key}>
-                            <TableCell className="text-large">{item.size}</TableCell>
-                            <TableCell className="text-large">{item.drink}</TableCell>
-                            <TableCell className="text-large">{item.complement}</TableCell>
-                            <TableCell className="text-large">{(item.chai || item.infusion) && `${item.chai}${item.infusion}`}</TableCell>
-                            <TableCell className="text-large">{item.type}</TableCell>
-                            <TableCell className="text-large">{(item.sugar_type || item.sugar_qty) && `${item.sugar_type} ${item.sugar_qty}`}</TableCell>
-                            <TableCell><Button color="success" onPress={() => removeFromList(item.key)}>Entregar</Button></TableCell>
-                        </TableRow>
+                        <TableCell className="text-large">{item.size}</TableCell>
+                        <TableCell className="text-large">{item.drink}</TableCell>
+                        <TableCell className="text-large">{item.complement}</TableCell>
+                        <TableCell className="text-large">{(item.chai || item.infusion) && `${item.chai}${item.infusion}`}</TableCell>
+                        <TableCell className="text-large">{item.type}</TableCell>
+                        <TableCell className="text-large">{(item.sugar_type || item.sugar_qty) && `${item.sugar_type} ${item.sugar_qty}`}</TableCell>
+                        <TableCell><Button color="success" onPress={removeFromList(item.key)}>Entregar</Button></TableCell>
+                    </TableRow>
                     )}
                 </TableBody>
             </Table>
